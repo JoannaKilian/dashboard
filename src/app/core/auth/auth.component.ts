39b81d42b81deedc6b21a166426e2e49fa4635cc
Joanna Borkala-Kilian <joannaborkala@gmail.com>
2023-07-26 14:46:35 +0200

    login component

--- src/app/core/auth/auth.component.ts
+++ src/app/core/auth/auth.component.ts
@@ -2,30 +2,59 @@ import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
- selector: 'app-auth',
- templateUrl: './auth.component.html',
- styleUrls: ['./auth.component.scss']
+    selector: 'app-auth',
+    templateUrl: './auth.component.html',
+    styleUrls: ['./auth.component.scss']
 })
 
 export class AuthComponent implements OnInit {
 
     authForm: FormGroup;
+    isLogin: boolean = true;
+    repeatPasswordError: boolean = false;
 
     constructor(private formBuilder: FormBuilder) { }
 
     ngOnInit(): void {
+        this.registerForm();
+        this.authForm.get('repeatPassword')?.valueChanges.subscribe(() => {
+            this.checkRepeatPassword();
+        });
+    }
+
+    registerForm() {
         this.authForm = this.formBuilder.group({
-            username: new FormControl ('', Validators.required),
-            password: new FormControl ('', Validators.required)
+            username: new FormControl('', [Validators.required, Validators.minLength(4)]),
+            password: new FormControl('', [Validators.required, Validators.minLength(4)]),
+            repeatPassword: new FormControl('', Validators.minLength(4))
         })
+    }
+
+    checkRepeatPassword() {
+        this.repeatPasswordError = this.authForm.get('password')?.value !== this.authForm.get('repeatPassword')?.value;
     }
 
     onSubmit() {
         if (this.authForm.invalid) {
             return;
-          }
-      
-          const username = this.authForm.get('username')?.value;
-          const password = this.authForm.get('password')?.value;
+        }
+
+        const username = this.authForm.get('username')?.value;
+        const password = this.authForm.get('password')?.value;
+
+        this.authForm.reset();
+    }
+
+    onSwitchLogin() {
+        this.isLogin = !this.isLogin;
+        if (this.isLogin) {
+            this.authForm.get('repeatPassword')?.clearValidators();
+        } else {
+            this.authForm.get('repeatPassword')?.addValidators([Validators.required])
+        }
+        this.repeatPasswordError = false;
+        this.authForm.patchValue({ password: '', repeatPassword: '' });
+        this.authForm.get('repeatPassword')?.markAsUntouched();
+        this.authForm.get('password')?.markAsUntouched();
     }
 }
