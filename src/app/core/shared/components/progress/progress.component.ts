0bfada4c855af9521bf451801cd2b65623b55a3c
Joanna Borkala-Kilian <joannaborkala@gmail.com>
2023-09-23 23:39:39 +0200

    custom days

--- src/app/core/shared/components/progress/progress.component.ts
+++ src/app/core/shared/components/progress/progress.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ProgressSpinnerMode } from '@angular/material/progress-spinner';
+import { of, switchMap } from 'rxjs';
+import { DaysAlertService } from 'src/app/core/services/days-alert.service';
 
 @Component({
   selector: 'app-progress',
@@ -14,9 +16,28 @@ export class ProgressComponent implements OnInit {
   progressValue: number;
   mode: ProgressSpinnerMode = 'determinate';
   alert: boolean;
+  daysAlertValue: number;
 
-  ngOnInit(){
-    this.alert = this.days > 30 ? false : true;
-    this.progressValue = this.days/365 * 100
+  constructor(
+    private daysAlertService: DaysAlertService,
+  ) {
+    daysAlertService.daysAlert$.subscribe(data => {
+      this.daysAlertValue = data
+    })
+  };
+
+  ngOnInit() {
+    if (this.daysAlertValue !== undefined) {
+      this.alert = this.days > this.daysAlertValue ? false : true;
+    } else {
+      this.daysAlertService.daysAlert$
+        .pipe(
+          switchMap(day => {
+            this.alert = this.days > day ? false : true;
+            return of(null);
+          })
+        ).subscribe()
+    }
+    this.progressValue = this.days / 365 * 100
   }
 }
